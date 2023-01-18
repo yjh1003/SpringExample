939256545544cf9cc20b6416ac5841c6ba17a7c6
JIZIM\ckfwl <d>
2023-01-18 21:49:53 +0900

    [허예진] - JSTL 예제1 정리

---
+++ src/main/java/com/yjh/spring/ex/jstl/JSTLController.java
@@ -0,0 +1,16 @@
+package com.yjh.spring.ex.jstl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jstl")
+public class JSTLController {
+
+	@GetMapping("/ex01")
+	public String ex01() {
+		return "jstl/ex01";
+	}
+	
+}
